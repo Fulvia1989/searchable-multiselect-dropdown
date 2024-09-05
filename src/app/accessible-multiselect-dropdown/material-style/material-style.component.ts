674ac29d167e5fe771129b7dddc48fccf0f4f3e3
fventuri <f.venturi@cineca.it>
2024-09-05 18:00:30 +0200

    material style multiselect

---
+++ src/app/accessible-multiselect-dropdown/material-style/material-style.component.ts
@@ -0,0 +1,176 @@
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KeyboardKey } from '../models/keyboard';
+import { OptionElement } from '../models/select';
+import { debounceTime, distinctUntilChanged, startWith } from 'rxjs';
+
+@Component({
+  selector: 'material-multiselect',
+  standalone: true,
+  imports: [
+    NgClass,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: '../multiselect-dropdown.component.html',
+  styleUrl: './material-style.component.scss'
+})
+export class MaterialComponent {
+  isDropdownOpen:boolean = false;
+  filter:FormControl = new FormControl<string>('');
+  currentOptionIndex:number = 0;
+
+  selected:OptionElement[] =[];
+  options:OptionElement[]=[];
+  @Input() set filteredElements(value:OptionElement[]){
+    this.currentOptionIndex = 0;
+    this.options = [   
+       {id:0, name:'all',label:'Select All',isActiveDescendant:true,selected:false},
+    ].concat(value);
+  };
+  
+  @Output() filterValueChange: EventEmitter<string | null> = new EventEmitter();
+  @Output() selectedChange: EventEmitter<OptionElement[]> = new EventEmitter();
+
+  @ViewChild('input') input!: ElementRef;
+  @ViewChild('dropdown') dropdown!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentInteraction = (event:any) => {
+
+    const isClickInsideButton = this.input.nativeElement.contains(event.target);
+    const isClickInsideDropdown = this.dropdown.nativeElement.contains(event.target);
+  
+    if (!isClickInsideButton && !isClickInsideDropdown && this.isDropdownOpen){
+      this.toggleDropdown();
+    }
+  };
+
+  constructor(){}
+  ngOnInit(): void {
+   this.filter.valueChanges.pipe(
+    startWith(''),
+    distinctUntilChanged()
+   ).subscribe(
+    value => {
+      console.log(value)
+      this.filterValueChange.emit(value);
+
+    }
+   )
+    
+  }
+
+  toggleDropdown = () => {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    if (this.isDropdownOpen) {
+      this.focusCurrentOption();
+    } else {
+
+      this.input.nativeElement.focus();
+    }
+  };
+  focusCurrentOption = () => {
+
+    let itemId = `item${this.currentOptionIndex}`;
+    const currentOption = document.getElementById(itemId);
+    console.log(currentOption);
+    currentOption?.focus();
+    currentOption?.scrollIntoView({
+      //block: 'nearest',
+      behavior: 'smooth',
+      block: 'center'
+    });
+  
+  };
+
+
+  handleKeyPress = (event:KeyboardEvent) => {
+    //event.preventDefault();
+   const { key } = event;
+   const openKeys:string[] = [KeyboardKey.Enter];
+ 
+   if (!this.isDropdownOpen && openKeys.includes(key) || (this.isDropdownOpen && key === KeyboardKey.Escape)) {
+     this.toggleDropdown();
+   }else if (this.isDropdownOpen) {
+    switch (key) {
+      case KeyboardKey.Escape:
+        this.toggleDropdown();
+        break;
+      case KeyboardKey.ArrowDown:
+        this.moveFocusDown();
+        break;
+      case KeyboardKey.ArrowUp:
+        this.moveFocusUp();
+        break;
+      case ' ':
+        this.selectCurrentOption(event);
+        break;
+      default:
+        break;
+    }
+  }
+  };
+
+  moveFocusDown = () => {
+
+    if (this.currentOptionIndex < this.options.length - 1) {
+      this.currentOptionIndex++;
+    } else {
+      this.currentOptionIndex = 0;
+    };
+    this.options.forEach((el,i) =>{
+      if(i == this.currentOptionIndex){
+        el.isActiveDescendant=true;
+      }else{
+        el.isActiveDescendant=false;
+      }
+    });
+ 
+    this.focusCurrentOption();
+  };
+  moveFocusUp = () => {
+
+    if (this.currentOptionIndex > 0) {
+      this.currentOptionIndex--;
+    } else {
+      this.currentOptionIndex = this.options.length - 1;
+    }
+    this.options.forEach(el =>{
+      if(el.id == this.currentOptionIndex){
+        el.isActiveDescendant=true;
+      }else{
+        el.isActiveDescendant=false;
+      }
+    });
+    this.focusCurrentOption();
+  };
+  
+
+  selectCurrentOption = (event:KeyboardEvent) => {
+    event.preventDefault();
+    let currentEl = this.options[this.currentOptionIndex];
+    currentEl.selected=!currentEl.selected;
+
+    if(!this.currentOptionIndex){
+      this.options.forEach(el => el.selected = currentEl.selected);
+      this.selected = [...this.options.filter(el => el.selected)];
+    }else{
+      if(currentEl.selected){
+        this.selected.push(currentEl);
+      }else{
+        this.selected = this.selected.filter(el => el.id !== currentEl.id);
+      }
+    };
+    this.selectedChange.emit(this.selected);
+ 
+
+
+  };
+
+
+  
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AsyncPipe } from '@angular/common';
 import { MultiselectComponent } from './multiselect/multiselect.component';
 import { MultiselectDropdownComponent } from './accessible-multiselect-dropdown/multiselect-dropdown.component';
 import { OptionElement } from './accessible-multiselect-dropdown/models/select';
+import { MaterialComponent } from "./accessible-multiselect-dropdown/material-style/material-style.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MultiselectDropdownComponent,AsyncPipe, MultiselectComponent],
+  imports: [RouterOutlet, MultiselectDropdownComponent, AsyncPipe, MultiselectComponent, MaterialComponent,MaterialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -29,12 +30,32 @@ export class AppComponent {
 
   );
 
+  updateList(value:string|null){
+    this.filter$.next(value);
+  }
   private _filter(value: string, array:OptionElement[]): OptionElement[] {
     const filterValue = value.toLowerCase();
     return array.filter(el => el.label.toLowerCase().includes(filterValue));
   }
+  fruits:OptionElement[]=[
+    {id:1, name:'apple',label:'Apple',isActiveDescendant:false,selected:false},
+    {id:2, name:'banana',label:'Banana',isActiveDescendant:false,selected:false},
+    {id:3, name:'grapes',label:'Grapes',isActiveDescendant:false,selected:false},
+    {id:4, name:'strawberry',label:'Strawberry',isActiveDescendant:false,selected:false},
+    {id:5, name:'pineapple',label:'Pineapple',isActiveDescendant:false,selected:false},
+  ];
+  fruitFilter$: BehaviorSubject<string|null> = new BehaviorSubject<string|null>(null);
+  filteredFruits$  = this.fruitFilter$.pipe(
+    startWith(null),
+    map((el: string | null) => (el ? this._filter(el, this.fruits) : this.fruits.slice())),
 
-  updateList(value:string|null){
-    this.filter$.next(value);
+  );
+  updateFruitList(value:string|null){
+    this.fruitFilter$.next(value);
   }
+
+
+
+
+
 }
